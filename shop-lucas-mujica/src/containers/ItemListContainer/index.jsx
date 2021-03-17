0622d287e11059bcd43dc8777535367b8fe037fe
Lucas Mujica <71916856+LukiMujica@users.noreply.github.com>
2021-03-17 01:19:01 -0300

    ItemCount

--- shop-lucas-mujica/src/containers/ItemListContainer/index.jsx
+++ shop-lucas-mujica/src/containers/ItemListContainer/index.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../App.css";
+import { ItemCount } from "../../components/ItemCount";
 
-export default function ItemListContainer(props) {
-  return <h1>Welcome, {props.greeting}</h1>;
-}
+const ItemListContainer = (props) => {
+  const [contador, setContador] = useState(1);
+
+  const onAdd = (stock) => {
+    console.log(stock);
+    if (contador < stock) {
+      setContador(contador + 1);
+    } else {
+      alert("No tenemos mas stock");
+    }
+  };
+
+  const onSubstract = (stock) => {
+    if (contador > 1) {
+      setContador(contador - 1);
+    } else {
+      alert("El valor es menor a lo que podemos vender");
+    }
+  };
+
+  return (
+    <>
+      <ItemCount
+        stock={12}
+        contador={contador}
+        onSubstract={onSubstract}
+        onAdd={onAdd}
+      />
+    </>
+  );
+};
+export default ItemListContainer;

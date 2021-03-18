22c6697e9bed41af7af47eddfce121133b07e6bf
Lucas Mujica <71916856+LukiMujica@users.noreply.github.com>
2021-03-18 13:01:04 -0300

    Buttons disabled

--- shop-lucas-mujica/src/App.jsx
+++ shop-lucas-mujica/src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import ListCardContainer from "./containers/ListCardContainer";
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./containers/ItemListContainer";
 
@@ -7,7 +6,6 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <ListCardContainer />
       <ItemListContainer greeting={"look here our catalog!"} />
     </div>
   );
--- shop-lucas-mujica/src/components/CardWidget/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const CartWidget = () => {
-  return (
-    <img style={{ width: "30px" }} src="./img/shopping-cart-solid.svg" alt="" />
-  );
-};
-
-export default CartWidget;

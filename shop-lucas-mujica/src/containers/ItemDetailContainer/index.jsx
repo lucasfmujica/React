ec77407a91672782261e29cc3c2fc547513f6502
Lucas Mujica <71916856+LukiMujica@users.noreply.github.com>
2021-03-19 00:33:26 -0300

    Card styles

---
+++ shop-lucas-mujica/src/containers/ItemDetailContainer/index.jsx
@@ -0,0 +1,14 @@
+// simular el tomar un producto a través de una API
+
+import React, { useEffect } from 'react'
+
+const ItemDetailContainer = (props) => {
+useEffect(() => {
+  setTimeout(() => {
+new Promise ()
+
+  },3000)
+return () => {}
+},[])
+
+export default ItemDetailContainer
